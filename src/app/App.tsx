d98b98c256d924302e8dba67a5cff2e86ea6356b
Ben Thiam <seydinat235@gmail.com>
2025-08-31 00:48:32 +0000

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1>Hello world</h1>
+      <p>Initaialisation de l'application vite js avec tailwind css</p>
+      <Button className="mt-4">Click Me</Button>
+    </div>
+  )
+}
+
+export default App
